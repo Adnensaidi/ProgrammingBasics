a1a3dc86a1935b931a9a5c7d829f71f63e5469aa
Adnen Saidi <adnensaidi8294@gmail.com>
2024-09-25 15:11:45 +0100

    Predict Arrays

---
+++ Week_1/Day_2/Core/Predict Arrays/PredictArrays.js
@@ -0,0 +1,30 @@
+var contactInfo = ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345];
+
+console.log(contactInfo);
+
+// 
+
+
+var produce = ["apples", "oranges"];
+var frozen = ["broccoli", "ice cream"];
+frozen.push("hashbrowns");
+console.log(frozen);
+// broccoli , ice cream , hashbrowns
+
+
+
+
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zorro");
+
+// var movieLibrary = ["Bambi", "E.T.", "Toy Story", "Zorro"];
+
+
+movieLibrary[1] = "Beetlejuice";
+
+
+// var movieLibrary = ["Bambi", "Beetlejuice", "Toy Story", "Zorro"];
+
+
+console.log(movieLibrary);
+//Bambi, Beetlejuice, Toy Story, Zorro
