0176d1867521849da8477b0369f5fe9408ddfd04
Adnen Saidi <adnensaidi8294@gmail.com>
2024-09-25 14:02:31 +0100

    Practice Commenting

--- Week_1/Day_2/Optional/Practice_Commenting/code.js
+++ Week_1/Day_2/Optional/Practice_Commenting/code.js
@@ -1,9 +1,10 @@
 // YOUR COMMENT HERE - what would this variable do?
-// 
+// the variable declares that likecount is equal to 0
 var likeCount = 0;
 // YOUR COMMENT HERE - what would be the function of this code block?
+// declare a function called increaselikes with no parameter
 function increaseLikes() {
     // YOUR COMMENT HERE - what is this sort operation for?
+    //this operation increase the likecount by 1 
     likeCount = likeCount + 1;
 }
-
